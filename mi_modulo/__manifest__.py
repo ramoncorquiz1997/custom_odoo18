58c99e2af750406799a38b89f7ae416d7bddb376
Ramon Contreras <ramoncorquiz12@gmail.com>
2025-12-27 03:58:13 +0000

    init custom addons

---
+++ mi_modulo/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Mi Modulo",
+    "version": "18.0.1.0.0",
+    "depends": ["base", "contacts"],  # contactos solo para el ejemplo
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
