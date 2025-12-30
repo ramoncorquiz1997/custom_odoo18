052301ae0e7bc9bbb69768aa14bc36602f2ea37f
rcontreras <rcontreras@aliquammed.com>
2025-12-30 15:31:58 -0800

    Cambio en la vrsion

--- mi_modulo/__manifest__.py
+++ mi_modulo/__manifest__.py
@@ -1,7 +1,7 @@
 
 {
     "name": "Aduana - Operaciones y Pedimentos",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "CRM",
     "summary": "Gestión de operaciones aduanales y pedimentos desde CRM",
     "depends": ["crm", "mail", "base", "contacts"],
